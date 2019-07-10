23352011b9b8780c301aea3bb3f046c32549f340
Mayank Sharma <mayank.sharma2410@gmail.com>
2019-07-10 12:02:04 +0530

    Add high and low to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         }else if(body.error){
             callback('Unable to find location', undefined)
         }else{
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' There is a ' + body.currently.precipProbability + '% chance of rain')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degree out. The high today is ' + body.daily.data[0].temperatureHigh + 'C, with a low of ' + body.daily.data[0].temperatureLow + 'C. There is a ' + body.currently.precipProbability + '% chance of rain')
         }
     })
 }
